4496f87ca18028b6ceb460d6df06ee2adc76449f
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-17 15:46:05 +0530

    7

---
+++ test7.py
@@ -0,0 +1,6 @@
+# Create a tuple of 7 weekdays and print the 3rd and last day using indexing.
+
+weekdays = ("Sunday", "Monday", "Tuesday" , "Wednesday", "Thrusday", "Friday", "Saturaday")
+
+print(weekdays[2]) # 3rd 
+print(weekdays[-1]) 
