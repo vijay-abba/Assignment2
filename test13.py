e3d654354e26e43cbb844aef52fe551539e9f154
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-17 18:27:26 +0530

    13

---
+++ test13.py
@@ -0,0 +1,21 @@
+# Create a dictionary of 5 countries and their capitals, then print all key-value pairs.
+
+
+countries_capitals = {
+    "India": "New Delhi",
+    "United States": "Washington, D.C.",
+    "United Kingdom": "London",
+    "Canada": "Ottawa",
+    "Australia": "Canberra",
+    "Germany": "Berlin",
+    "France": "Paris",
+    "Japan": "Tokyo",
+    "China": "Beijing",
+    "Brazil": "Brasília",
+    "Russia": "Moscow",
+    "Italy": "Rome"
+}
+
+
+for [country, capital] in countries_capitals.items():
+    print(f"Capital of {country} is {capital}")
