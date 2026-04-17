62f2457a2d8ff8995f8b72870e20213c613c1d86
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-17 18:08:42 +0530

    10

---
+++ test10.py
@@ -0,0 +1,29 @@
+# Given a tuple of numbers, return a new tuple containing only the prime numbers.
+
+
+def is_prime(number):
+    is_prime_numbere = True
+    if number >= 2 :
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime_numbere =  False
+                break
+        return is_prime_numbere
+    else:
+        is_prime_numbere = False
+        return is_prime_numbere
+
+
+def prime_tuple(my_tuble):
+    list_a = []
+    for item in my_tuble:
+        if(is_prime(item)):
+            list_a.append(item)
+    return tuple(list_a)
+
+
+
+tuple_a = tuple(range(1, 100))
+
+result = prime_tuple(tuple_a)
+print(result)
