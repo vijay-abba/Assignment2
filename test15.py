3ab7c850ef8372a6e05dc6f54152f483e5436a95
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-17 19:01:21 +0530

    15

--- test15.py
+++ test15.py
@@ -1,1 +1,17 @@
 # Write a function that takes a sentence and returns a dictionary with word frequency counts (case-insensitive).
+
+
+def word_frequ(my_sentance):
+
+    freq_dict = {}
+
+    formated = my_sentance.lower().strip()
+    for word in formated.split(" "):
+        freq_dict[word] = formated.count(word)
+    return freq_dict
+
+
+# sent = "apple banana apple orange banana apple"
+sent = "Apple apple APPLE banana BANANA banana grape "
+
+print(word_frequ(sent))
