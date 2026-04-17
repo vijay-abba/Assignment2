6d5920947cb7cd51095d08db9991c6863d83f71c
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-17 18:15:39 +0530

    11

---
+++ test11.py
@@ -0,0 +1,17 @@
+# Write a function that takes a tuple of strings and returns a tuple with the length of each string.
+
+
+def len_of_stiring_tup(my_tuple):
+    len_tuple = []
+    for word in my_tuple:
+        len_tuple.append(len(word))
+    return tuple(len_tuple)
+
+
+
+
+fruits = ("apple", "bananna", "graps", "pappay", "watermelon", "guava", "pinneapple")
+
+result = len_of_stiring_tup(fruits)
+
+print(result)
