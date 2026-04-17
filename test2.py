f89a9979f23054fca05c342d6ffa6d02448282bc
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-17 15:19:51 +0530

    test2 completed

---
+++ test2.py
@@ -0,0 +1,34 @@
+# Write a program to count how many even and odd numbers are in a list. 
+
+def find_even_and_odd_count(my_list):
+
+    even_numbers = []
+    odd_numbers = []
+
+    for number in my_list:
+        even_numbers.append(number) if number % 2 == 0 else odd_numbers.append(number)
+
+    return f"In the given list {my_list} there are {len(even_numbers)} Even, and {len(odd_numbers)} Odd numbers"
+
+list_a = [1, 2, 3, 4, 5, 6]
+list_b = [10, 15, 20, 25, 30]
+list_c = [7, 9, 11, 13]
+list_d = [2, 4, 6, 8, 10, 12]
+list_e = [0, 1, 2, 3, 4]
+list_f = [-1, -2, -3, -4, -5]
+list_g = [21, 22, 23, 24, 25, 26, 27]
+list_h = [100]
+list_i = [101]
+list_j = []
+
+
+print(find_even_and_odd_count(list_a))
+print(find_even_and_odd_count(list_b))
+print(find_even_and_odd_count(list_c))
+print(find_even_and_odd_count(list_d))
+print(find_even_and_odd_count(list_e))
+print(find_even_and_odd_count(list_f))
+print(find_even_and_odd_count(list_g))
+print(find_even_and_odd_count(list_h))
+print(find_even_and_odd_count(list_i))
+print(find_even_and_odd_count(list_j))
