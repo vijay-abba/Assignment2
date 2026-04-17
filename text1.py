045abb1e902cd41e68660260a299a815a4274fdc
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-17 15:04:28 +0530

    completed 1st test

---
+++ text1.py
@@ -0,0 +1,17 @@
+# Find the largest and smallest number in a list [23, 45, 12, 67, 89, 34] without using max() or min().
+
+my_list_a = [23, 45, 12, 67, 89, 34]
+
+def find_min_and_max(my_list):
+    smallest = my_list[0]
+    largest  = my_list[0]
+    for number in my_list:
+        if number < smallest:
+            smallest = number
+        elif number > largest:
+            largest = number
+    return f"Smallest: {smallest}, Largest: {largest}"
+
+
+result = find_min_and_max(my_list_a)
+print(result)
